c5edfc08e03dbee0e510aef3036079adc774025a
Dr4kk0nnys <renatoversianidrakk@gmail.com>
2020-03-27 09:12:26 -0300

    update method added

--- main.py
+++ main.py
@@ -47,6 +47,36 @@ class Database():
         for value in data:
             self.write(value)
 
+    def update(self, index, new_data):
+
+        file = open(self.database_name, 'r')
+
+        # lines = all the characters in the file, splited by a line breaker
+        # without the last line (always an empty line)
+        lines = file.read().split('\n')[:-1]
+        # file.read returns all the characters, instead of all the lines
+
+        if (index >= len(lines)):
+            print('Unable to remove such index')
+            return
+
+        data = []
+
+        # passing all the existing data to the array
+        # except for the one, on the index we want to remove
+        for i in range(len(lines)):
+            if (i != index):
+                data.append(lines[i])
+            else:
+                data.append(new_data)
+
+        # erasing all the data from the database file
+        file = open(self.database_name, 'w')
+
+        # passing all the data from the array to the fresh database
+        for value in data:
+            self.write(value)
+
 
 if __name__ == '__main__':
     database = Database('database.txt')
